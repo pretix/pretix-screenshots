5d491aa8f4ef9b8ec1e68069b5dff3af00fe8315
Raphael Michel <mail@raphaelmichel.de>
2017-12-06 16:56:12 +0100

    Screenshots of all settings pages

--- scenes/docs/event/scene_event_settings.py
+++ scenes/docs/event/scene_event_settings.py
@@ -17,3 +17,43 @@ def shot_payment_settings(live_server, organizer, event, logged_in_client):
         organizer.slug, event.slug
     ))
     screenshot(logged_in_client, 'event/settings_payment.png')
+
+
+@pytest.mark.django_db
+def shot_plugin_settings(live_server, organizer, event, logged_in_client):
+    logged_in_client.get(live_server.url + '/control/event/{}/{}/settings/plugins'.format(
+        organizer.slug, event.slug
+    ))
+    screenshot(logged_in_client, 'event/settings_plugins.png')
+
+
+@pytest.mark.django_db
+def shot_display_settings(live_server, organizer, event, logged_in_client):
+    logged_in_client.get(live_server.url + '/control/event/{}/{}/settings/display'.format(
+        organizer.slug, event.slug
+    ))
+    screenshot(logged_in_client, 'event/settings_display.png')
+
+
+@pytest.mark.django_db
+def shot_tickets_settings(live_server, organizer, event, logged_in_client):
+    logged_in_client.get(live_server.url + '/control/event/{}/{}/settings/tickets'.format(
+        organizer.slug, event.slug
+    ))
+    screenshot(logged_in_client, 'event/settings_tickets.png')
+
+
+@pytest.mark.django_db
+def shot_email_settings(live_server, organizer, event, logged_in_client):
+    logged_in_client.get(live_server.url + '/control/event/{}/{}/settings/email'.format(
+        organizer.slug, event.slug
+    ))
+    screenshot(logged_in_client, 'event/settings_email.png')
+
+
+@pytest.mark.django_db
+def shot_invoice_settings(live_server, organizer, event, logged_in_client):
+    logged_in_client.get(live_server.url + '/control/event/{}/{}/settings/invoice'.format(
+        organizer.slug, event.slug
+    ))
+    screenshot(logged_in_client, 'event/settings_invoice.png')
